cb770a2f4ae956a4311a8344c307b1c0b57eb939
michael <nkumichael1@gmail.com>
2026-04-01 20:24:24 +0100

    moved to latest nextjs

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <p>
+        This is the account page. You can view and edit your account details
+        here.
+      </p>
+    </div>
+  );
 };
 
 export default page;
